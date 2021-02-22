429630bbf5192d22a8a3ccef499966910e493fae
Harish B <76545288+harish-1998@users.noreply.github.com>
2021-02-22 09:08:38 +0530

    Add files via upload

---
+++ comments.sql
@@ -0,0 +1,17 @@
+prompt --application/comments
+begin
+--   Manifest
+--     APPLICATION COMMENTS: 28934
+--   Manifest End
+wwv_flow_api.component_begin (
+ p_version_yyyy_mm_dd=>'2020.10.01'
+,p_release=>'20.2.0.00.20'
+,p_default_workspace_id=>33254518917656529070
+,p_default_application_id=>28934
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_HARISH0031998'
+);
+null;
+wwv_flow_api.component_end;
+end;
+/
---
+++ plugin_settings.sql
@@ -0,0 +1,64 @@
+prompt --application/plugin_settings
+begin
+wwv_flow_api.component_begin (
+ p_version_yyyy_mm_dd=>'2020.10.01'
+,p_release=>'20.2.0.00.20'
+,p_default_workspace_id=>33254518917656529070
+,p_default_application_id=>28934
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_HARISH0031998'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498550492204989259)
+,p_plugin_type=>'ITEM TYPE'
+,p_plugin=>'NATIVE_COLOR_PICKER'
+,p_attribute_01=>'modern'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498550799533989259)
+,p_plugin_type=>'ITEM TYPE'
+,p_plugin=>'NATIVE_RICH_TEXT_EDITOR'
+,p_attribute_01=>'Y'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498551054284989260)
+,p_plugin_type=>'ITEM TYPE'
+,p_plugin=>'NATIVE_SINGLE_CHECKBOX'
+,p_attribute_01=>'Y'
+,p_attribute_02=>'N'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498551396317989260)
+,p_plugin_type=>'ITEM TYPE'
+,p_plugin=>'NATIVE_STAR_RATING'
+,p_attribute_01=>'fa-star'
+,p_attribute_04=>'#VALUE#'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498551626778989260)
+,p_plugin_type=>'ITEM TYPE'
+,p_plugin=>'NATIVE_YES_NO'
+,p_attribute_01=>'Y'
+,p_attribute_03=>'N'
+,p_attribute_05=>'SWITCH_CB'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498551911750989261)
+,p_plugin_type=>'REGION TYPE'
+,p_plugin=>'NATIVE_CSS_CALENDAR'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498552236289989261)
+,p_plugin_type=>'REGION TYPE'
+,p_plugin=>'NATIVE_DISPLAY_SELECTOR'
+,p_attribute_01=>'Y'
+);
+wwv_flow_api.create_plugin_setting(
+ p_id=>wwv_flow_api.id(33498552530414989261)
+,p_plugin_type=>'REGION TYPE'
+,p_plugin=>'NATIVE_IR'
+,p_attribute_01=>'IG'
+);
+wwv_flow_api.component_end;
+end;
+/
